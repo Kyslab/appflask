65da31e3b8fd4d5ecbb6f75620a38a194d856b1e
Doan Ky <doanvanky36k21@gmail.com>
2024-07-05 22:37:30 +0700

    Update requirements and database configuration

--- app.py
+++ app.py
@@ -2,18 +2,26 @@
 # app.config['MAIL_USERNAME'] = 'mergexceltool'
 # app.config['MAIL_PASSWORD'] = 'uhuu dnwi kaiu days'
 # app.config['MAIL_DEFAULT_SENDER'] = 'mergexceltool@gmail.com'
+
+    # client_id='206611615101-g9kp571dagj69qn1b0ffb723c8qn9d7q.apps.googleusercontent.com',
+    # client_secret='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',
+import secrets
 import os
-import random
-import string
 from flask import Flask, request, redirect, url_for, send_file, render_template, flash, session
 from werkzeug.utils import secure_filename
 import openpyxl
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Length, Email, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import InputRequired, Length, Email, ValidationError,EqualTo
 from flask_mail import Mail, Message
+from random import randint
+from urllib.parse import quote as url_quote
+from authlib.integrations.flask_client import OAuth
+from authlib.integrations.base_client.errors import OAuthError
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
@@ -39,6 +47,18 @@ app.config['MAIL_DEFAULT_SENDER'] = 'mergexceltool@gmail.com'
 
 mail = Mail(app)
 
+# OAuth configuration
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id='206611615101-g9kp571dagj69qn1b0ffb723c8qn9d7q.apps.googleusercontent.com',
+    client_secret='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',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
+
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 if not os.path.exists(app.config['PROCESSED_FOLDER']):
@@ -54,7 +74,7 @@ class User(db.Model):
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(), Length(max=150)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=150)])
-
+    
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
@@ -64,10 +84,59 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(), Length(max=150)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=150)])
     remember = BooleanField('Remember me')
-
+    
 class ConfirmEmailForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(), Length(max=150)])
     code = StringField('Confirmation Code', validators=[InputRequired(), Length(min=6, max=6)])
+
+s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=150)])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('New Password', validators=[InputRequired(), Length(min=4, max=150)])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+def send_reset_email(user):
+    token = s.dumps(user.email, salt='password-reset-salt')
+    msg = Message('Password Reset Request', sender='your-email@gmail.com', recipients=[user.email])
+    link = url_for('reset_token', token=token, _external=True)
+    msg.body = f'Please click the following link to reset your password: {link}'
+    mail.send(msg)
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if 'user_id' in session:
+        return redirect(url_for('upload_and_list_files'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    try:
+        email = s.loads(token, salt='password-reset-salt', max_age=3600)
+    except SignatureExpired:
+        flash('The token is expired!', 'danger')
+        return redirect(url_for('reset_request'))
+
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=email).first()
+        if user:
+            user.password = form.password.data
+            db.session.commit()
+            flash('Your password has been updated! You can now log in.', 'success')
+            return redirect(url_for('login'))
+    return render_template('reset_token.html', form=form)
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -77,47 +146,87 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = form.password.data
-        confirmation_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-        session['confirmation_code'] = confirmation_code
-        session['email'] = form.email.data
-        session['password'] = hashed_password
+        new_user = User(email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
 
-        msg = Message('Email Confirmation', recipients=[form.email.data])
-        msg.body = f'Your confirmation code is {confirmation_code}'
+        verification_code = randint(100000, 999999)
+        session['email_verification'] = {'email': new_user.email, 'code': verification_code}
+
+        msg = Message('Email Verification', sender='your-email@gmail.com', recipients=[new_user.email])
+        msg.body = f'Your verification code is {verification_code}.'
         mail.send(msg)
 
-        return redirect(url_for('confirm_email'))
-
+        flash('Account created successfully. Please check your email for the verification code.', 'success')
+        return redirect(url_for('confirm_email', email=new_user.email))
     return render_template('register.html', form=form)
 
 @app.route('/confirm_email', methods=['GET', 'POST'])
 def confirm_email():
-    form = ConfirmEmailForm(email=session.get('email'))
-    error = None
+    if 'email_verification' not in session:
+        return redirect(url_for('register'))
+
+    email = session['email_verification']['email']
+    form = ConfirmEmailForm(email=email)
+
     if form.validate_on_submit():
-        if form.code.data == session.get('confirmation_code'):
-            new_user = User(email=form.email.data, password=session.get('password'), is_confirmed=True)
-            db.session.add(new_user)
+        if form.code.data == str(session['email_verification']['code']):
+            user = User.query.filter_by(email=email).first()
+            user.is_confirmed = True
             db.session.commit()
-            flash('Account confirmed successfully. Please log in.', 'success')
+            session.pop('email_verification', None)
+            flash('Email confirmed successfully. Please log in.', 'success')
             return redirect(url_for('login'))
         else:
-            error = 'Invalid confirmation code. Please try again.'
-
-    return render_template('confirm_email.html', form=form, error=error)
+            flash('Invalid verification code. Please try again.', 'danger')
+    return render_template('confirm_email.html', form=form, email=email)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+
+        if len(form.password.data) < 4:
+            flash('Password must be at least 4 characters long. Please check your password or log in with Google.', 'danger')
+            return render_template('login.html', form=form)
+              
         if user and user.password == form.password.data:
             session['user_id'] = user.id
-            flash('Logged in successfully', 'success')
+            # flash('Logged in successfully', 'success')
             return redirect(url_for('upload_and_list_files'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password or log in with Google.', 'danger')
     return render_template('login.html', form=form)
+
+@app.route('/login/google')
+def login_google():
+    nonce = secrets.token_urlsafe()
+    session['nonce'] = nonce
+    redirect_uri = url_for('authorize_google', _external=True)
+    return google.authorize_redirect(redirect_uri, nonce=nonce)
+@app.route('/authorize/google')
+def authorize_google():
+    try:
+        token = google.authorize_access_token()
+        nonce = session.pop('nonce', None)
+        resp = google.parse_id_token(token, nonce=nonce)
+        email = resp['email']
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            user = User(email=email, is_confirmed=True)
+            db.session.add(user)
+            db.session.commit()
+
+        session['user_id'] = user.id
+        flash('Logged in successfully with Google.', 'success')
+        return redirect(url_for('upload_and_list_files'))
+    except OAuthError as e:
+        print(e.error)
+        flash('Could not authorize with Google. Please try again.', 'danger')
+        return redirect(url_for('login'))
+
 
 @app.route('/logout')
 def logout():
