9595805d804a57f9eed3c0a600e69ca2b89d309e
Doan Ky <doanvanky36k21@gmail.com>
2024-07-22 22:46:16 +0700

    U

--- app.py
+++ app.py
@@ -241,8 +241,8 @@ def login():
 def login_google():
     nonce = secrets.token_urlsafe()
     session['nonce'] = nonce
-    # redirect_uri = url_for('authorize_google', _external=True, _scheme='https')
-    redirect_uri = url_for('authorize_google', _external=True, _scheme='http')
+    redirect_uri = url_for('authorize_google', _external=True, _scheme='https')
+    # redirect_uri = url_for('authorize_google', _external=True, _scheme='http')
     return google.authorize_redirect(redirect_uri, nonce=nonce)
 @app.route('/authorize/google')
 def authorize_google():
