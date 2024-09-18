30f1903a8b79640e219b2ee10e8adaac85085223
Doan Ky <doanvanky36k21@gmail.com>
2024-09-18 11:31:00 +0700

    supbase git ...

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ app.config['ALLOWED_EXTENSIONS'] = {'xlsx','xlsm'}
 #         return redirect(url, code=301)
     
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u7tbi7nfum7v0:pf5f201f6f9d9fbcc433d951c97a2556850b548a66deed2297b7a41bfe789d0b6@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7ctfli13pgsc0'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u7tbi7nfum7v0:pf5f201f6f9d9fbcc433d951c97a2556850b548a66deed2297b7a41bfe789d0b6@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7ctfli13pgsc0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.auigntrplygnjajymajy:Zoo8seekZoo8seek.&@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
